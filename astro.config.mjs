744831895914bcf48695a01febab284002239a4a
hotelll <283705946@qq.com>
2024-03-10 23:32:01 +0800

    1

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://hotelll.github.io',
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+      remarkPlugins: [
+          'remark-math',
+      ],
+      rehypePlugins: [
+          ['rehype-katex', {
+          // Katex plugin options
+          }]
+      ]
+  }
+});
